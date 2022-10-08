88b6ebb1c5e58442d36d1c7a7d369a4f5b0da175
Rohit Karande <rohitkarande543@gmail.com>
2022-10-08 10:22:14 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,15 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
-class App extends Component {
-    constructor(props) {
-		super(props);
-	    this.state = {
-	    text: "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy",
-		    click : 0
-	    
-	    }
-	};
-
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-	<button id="click" onClick={() => this.setState({count: this.state.count + 1 })}>Button</button>
-    <h6 id="para">{this.state.text}</h6>
-    		</div>
-    	);
-    }
-}
-
+const App = () => {
+  return (
+    <div id="main">
+      <h1 data-ns-test="project-name">Photo Gallery Application</h1>
+      <p data-ns-test="project-description">
+        I want to build Photo Gallery Application in React
+      </p>
+    </div>
+  );
+};
 
 export default App;
